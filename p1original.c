0f873a2a4742ed0454798a7ccc42593380364e70
ADARSH MENON <88380337+Adarsh-Menon@users.noreply.github.com>
2022-01-31 05:41:18 +0000

    P1 DONE 31/01/22

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
+#include<stdio.h>
 
+void input(int *a,int *b){
+  printf("Enter 2 numbers\n");
+  scanf("%d%d",a,b);
+}
+
+void add(int a,int b ,int *sum){
+  *sum=a+b;
+}
+
+void output(int a,int b,int sum){
+  printf("the sum %d + %d = %d",a,b,sum);
+}
+
+int main(){
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
